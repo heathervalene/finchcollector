03dcb956f1c93af377aaeff414dcc5e5b97769dd
HeatherRouke <heather214@gmail.com>
2024-01-09 12:12:16 -0500

    part 1 finch lab

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+finches = [
+  {'name': 'Crested bunting', 'distribution': 'Southeast Asia', 'habitat': 'subtropic', 'color': 'black and red', 'description': 'little jerk'},
+  {'name': 'Yellowhammer', 'distribution': 'Europe', 'habitat': 'dry, open country', 'color': 'yellow', 'description': 'annoying AF'},
+]
+
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
