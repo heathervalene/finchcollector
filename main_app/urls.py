1d3d9ba37f315f1828860d3c01946967f0b19526
HeatherRouke <heather214@gmail.com>
2024-01-12 14:46:21 -0500

    add photo upload feature

--- main_app/urls.py
+++ main_app/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
     path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
     path('finches/<int:finch_id>/diss_toy/<int:toy_id>/', views.diss_toy,name='diss_toy'),
+    path('finches/<int:finch_id>/add_photo/', views.add_photo, name='add_photo'),
 
 	
  ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,11 @@
+import uuid
+import boto3
+import os
+
 from django.shortcuts import render, redirect
 from django.views.generic.edit import View, CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Finch, Toy
+from .models import Finch, Toy, Photo
 from .forms import SightingForm
 
 
@@ -35,6 +39,23 @@ def assoc_toy(request, finch_id, toy_id):
 def diss_toy(request, finch_id, toy_id):
   Finch.objects.get(id=finch_id).toys.remove(toy_id)
   return redirect('detail', finch_id=finch_id)
+
+def add_photo(request, finch_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            bucket = os.environ['S3_BUCKET']
+            s3.upload_fileobj(photo_file, bucket, key)
+          
+            url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+          
+            Photo.objects.create(url=url, finch_id=finch_id)
+        except Exception as e:
+            print('An error occurred uploading file to S3')
+            print(e)
+    return redirect('detail', finch_id=finch_id)
 
 def add_sighting(request, finch_id):
     form = SightingForm(request.POST)
